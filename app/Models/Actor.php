19dc06ec1929b237334eba982e61dce15825d6dc
David Segerbo <david.k.segerbo@gmail.com>
2025-02-03 22:44:16 +0100

    reformated seed files

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actor extends Model
 {
-    //
+    protected $table = 'actor';
+    protected $primaryKey = 'actor_id';
+
+    public function genre()
+    {
+        return $this->belongsToMany(Genre::class, 'actor_genre', 'genre_id', 'actor_id');
+    }
+
+    public function movie()
+    {
+        return $this->belongsToMany(Movie::class, 'movie_actor', 'actor_id', 'movie_id');
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -13,4 +13,9 @@ class Movie extends Model
     {
         return $this->belongsToMany(Genre::class, 'movie_genre', 'movie_id', 'genre_id');
     }
+
+    public function actor()
+    {
+        return $this->belongsToMany(Actor::class, 'movie_actor', 'actor_id', 'movie_id');
+    }
 }
