d36b856cd27724369767ee1124352129912fb915
David Segerbo <david.k.segerbo@gmail.com>
2025-02-07 15:04:15 +0100

    Update Review.php
    Added protected primary key, sence we are using custom primary keys and not just ID

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $table = 'reviews';
+    protected $primaryKey = 'review_id';
     protected $fillable = ['movie_id', 'username', 'content', 'rating'];
 
     public function movie()
