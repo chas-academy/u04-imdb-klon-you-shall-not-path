918c09d0ef2f4771250d86d1974c1dfbbb3e476c
Sue-Holding <sue.holding55@gmail.com>
2025-02-10 21:04:15 +0100

    added responsive styling to admin-dashboard view and component pending-review-table

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -2,14 +2,16 @@
 
 @section('content')
 
-<div class="flex h-full">
-<div class="flex h-screen">
+<!-- <div class="flex h-full">
+<div class="flex h-screen"> -->
+<div class="flex flex-col md:flex-row h-full min-h-screen">
 
 
     <!-- Sidebar -->
+    <div class="flex flex-col md:flex-row h-full min-h-screen">    
     <x-admin-sidebar>
     </x-admin-sidebar>  
-
+    </div>
     
         <!-- Main Content Area -->
         <main class="flex-1 p-6 flex flex-col items-center w-full">
@@ -18,7 +20,7 @@
             <h2 class="text-2xl font-semibold text-white mb-4 text-center">Welcome, {{ $id->name }}</h2>
     
     <x-pending-reviews-table>
-    </x-pending-reiviews-table>    
+    </x-pending-reviews-table>    
 
         </main>
 
