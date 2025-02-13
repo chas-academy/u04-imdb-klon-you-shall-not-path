330b8090abfcfc2e49dcbd9bcfa74af64bd5e782
David Segerbo <david.k.segerbo@gmail.com>
2025-02-13 06:39:11 +0100

    updated styling to a version that functions

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-<!-- <div class="flex h-full">
-<div class="flex h-screen"> -->
-<div class="flex flex-col md:flex-row h-full min-h-screen">
+<div class="flex h-full">
+<div class="flex h-screen">
+<!-- <div class="flex flex-col md:flex-row h-full min-h-screen"> -->
 
     <!-- Sidebar -->
     <div class="md:w-1/4 w-full md:block hidden">
@@ -13,7 +13,7 @@
     </div>
     
         <!-- Main Content Area -->
-        <main class="flex-1 p-6 flex flex-col items-center w-full">
+        <main class="flex-1 p-6 flex items-center w-full">
 
             <!-- H1 Centered at the Top -->
             <h2 class="text-2xl font-semibold text-white mb-4 text-center">Welcome, {{ $id->name }}</h2>
@@ -25,6 +25,7 @@
         </main>
 
     </div>   
+</div>
 </div>
 
 @endsection
