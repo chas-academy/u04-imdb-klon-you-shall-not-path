338171b4ca595da2647112ff1e27d45cab6be2d7
Sue-Holding <sue.holding55@gmail.com>
2025-02-14 11:20:27 +0100

    changed sidebar to hidden on mobile view

--- resources/views/user-dashboard.blade.php
+++ resources/views/user-dashboard.blade.php
@@ -2,31 +2,21 @@
 
 @section('content')
 
-<div class="flex h-full">
-<div class="flex h-screen">
+<div class="min-h-screen flex flex-col">
+<div class=" flex flex-1">
 
-     
     <!-- Sidebar -->
-    <x-user-sidebar>
-    </x-user-sidebar>
+    <div class="md:w-1/4 w-full md:block hidden">
+        <x-admin-sidebar />
+    </div>
 
-    <!-- Main Content Area -->
     <main class="flex-1 p-6 flex flex-col items-center w-full">
         
-        <!-- H2 Centered at the Top -->
-        <h2 class="text-2xl font-semibold text-white mb-4 text-center">Welcome, John Doe</h2>
-         
-            <div class="flex w-full items-start">
+<       <h2 class="text-2xl font-semibold text-white mb-4 text-center">WatchList</h2>
+           
+<!-- Table for WatchList -->
 
-    <!-- Profile Image -->
-  
-        
-    <x-watchlist-gallery>
-    </x-watchlist-gallery>      
-
-            </div>
     </main>
-
 </div>
 </div>
 
